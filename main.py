a37acf87338db3708151c64d9461ee318c9fd401
flotzilla <null.0.byte@gmail.com>
2017-08-27 21:38:28 +0300

    fix exception handling

--- main.py
+++ main.py
@@ -65,8 +65,8 @@ if __name__ == '__main__':
 
     except KeyboardInterrupt:
         print(u"\r\n Bye")
-    except Exception, e:
-        logger.error("Whoops", e)
+    except Exception as e:
+        logger.error("Whoops", e.message)
     finally:
         utils.set_config_param('last_station', mc.player.get_current_station())
         utils.save_config_to_file()
